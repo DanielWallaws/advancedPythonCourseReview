4ab9236616cb0e9716880bff39aa70d90d5e24db
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2023-04-28 19:25:21 -0300

    uploading review assignments reaching class 14

---
+++ basic-python/aula2.py
@@ -0,0 +1,7 @@
+# \r\n ----> CRLF No canto inferior do VSCODE (quebra de linha característica do windows)
+#\n ----> QUebra de linha para sistema baseados em UNIX
+print(12,13, sep=" DANIEL ")
+print(30,513, sep=" DANIEL ", end='#')
+print(13,130, sep=" DANIEL ")
+print(17,1377, sep=" DANIEL ")
+print(182,155, sep=" DANIEL ")
---
+++ basic-python/aula3.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape -  Utilizado para imprimir algo que não quero que seja interpretado, apenas impresso.
+print("Luiz \"Otávio\"")
+
+# r
